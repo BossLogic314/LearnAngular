de11628210a6a3c53f8575809960ee2bf5d42811
Anish <anish.b@jarustech.com>
2025-04-11 19:35:27 +0530

    Two way data binding

--- wishlist_app/src/app/app.component.ts
+++ wishlist_app/src/app/app.component.ts
@@ -1,23 +1,31 @@
 import { Component } from '@angular/core';
 import { WishItem } from '../models/WishItem';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'wishlist_app';
 
-  wishList = [
+  wishList : WishItem[] = [
     new WishItem("Learn angular", false),
     new WishItem("Watch football", true),
     new WishItem("Start exercising", false),
     new WishItem("Watch movies", false)
   ];
+
+  newWishText : string = "";
+
+  createNewWishItem()
+  {
+    this.wishList.push(new WishItem(this.newWishText, false));
+    this.newWishText = "";
+  }
 
   toggleWishItem(item : WishItem, items : WishItem[])
   {
